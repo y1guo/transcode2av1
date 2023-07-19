56276a4218b8bde9be9d9afc4d75a6658e9ef28f
y1guo <guoyi0328@gmail.com>
2023-07-18 17:27:32 -0700

    basically working

--- transcode.py
+++ transcode.py
@@ -1,44 +1,67 @@
-import os, ffmpeg, json
+import os, ffmpeg, json, time, datetime
 from multiprocessing import Pool
+from colorama import Fore, Style
 
 
 # load config
 with open("config.json") as f:
     config = json.load(f)
-    IN_DIR_LIST = config["in_dir_list"]
+    IN_DIRS = config["in_dirs"]
     OUT_DIR = config["out_dir"]
-    LOW_QUALITY_ROOMIDS = config["low_quality_roomids"]
+    ROOMIDS = config["roomids"]
+    CONSTANT_QUALITY = config["constant_quality"]
+
+
+def msg(message: str):
+    print(f"{datetime.datetime.now().strftime('%H:%M:%S')} {message}")
 
 
 def transcode(in_file: str, out_file: str):
     assert in_file != out_file
-    # probe the input file
+    # get duration
     probe = ffmpeg.probe(in_file)
-    video_bitrate, audio_bitrate = 0, 0
-    for stream in probe["streams"]:
-        if stream["codec_type"] == "video":
-            video_bitrate = int(stream["bit_rate"])
-        elif stream["codec_type"] == "audio":
-            audio_bitrate = int(stream["bit_rate"])
-    # get quality factor based on roomid
-    roomid = os.path.basename(in_file).split("_")[0]
-    if roomid in LOW_QUALITY_ROOMIDS:
-        quality_factor = 51
-    else:
-        quality_factor = 30
+    duration = float(probe["format"]["duration"])
+    # transcode
+    start_time = time.perf_counter()
+    msg(f"{Fore.GREEN}Transcoding {in_file}{Style.RESET_ALL}")
     ffmpeg.input(in_file, hwaccel="cuda", hwaccel_output_format="cuda").output(
-        out_file, vcodec="av1_nvenc", cq=quality_factor, acodec="copy"
-    ).run(capture_stdout=False, capture_stderr=False, overwrite_output=True)
+        out_file, vcodec="av1_nvenc", cq=CONSTANT_QUALITY, acodec="copy"
+    ).run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
+    end_time = time.perf_counter()
+    speed = duration / (end_time - start_time)
+    # check compression ratio
+    in_size = os.path.getsize(in_file)
+    out_size = os.path.getsize(out_file)
+    ratio = out_size / in_size
+    if ratio < 1:
+        msg(
+            f"{Fore.GREEN}Compressed {in_file} from {in_size} to {out_size} ({ratio * 100:.0f}%), Speed ({speed:.1f}X){Style.RESET_ALL}"
+        )
+    else:
+        msg(
+            f"{Fore.YELLOW}Compressed {in_file} from {in_size} to {out_size} ({ratio * 100:.0f}%), Speed ({speed:.1f}X){Style.RESET_ALL}"
+        )
+        os.remove(out_file)
+        msg(f"{Fore.YELLOW}Removed {out_file}{Style.RESET_ALL}")
 
 
 # loop over all files in the directory list
-for dir in IN_DIR_LIST:
+args = []
+for dir in IN_DIRS:
     for file in os.listdir(dir):
         base_name, ext = os.path.splitext(file)
         if ext in [".flv", ".mp4", ".m4v"]:
+            # filter by roomid
+            roomid = base_name.split("_")[0]
+            if not roomid in ROOMIDS:
+                continue
             # get the full path of the file
             in_file = os.path.join(dir, file)
             # create the output file name
             out_file = os.path.join(OUT_DIR, base_name + ".mp4")
-            # transcode
-            transcode(in_file, out_file)
+            # add to the list of arguments
+            args.append((in_file, out_file))
+
+# transcode in parallel
+with Pool(4) as pool:
+    pool.starmap(transcode, args)
