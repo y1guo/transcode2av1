f63551a07520fe0b01a4d32e24cfbc9cb00973f4
y1guo <guoyi0328@gmail.com>
2023-09-11 22:51:59 -0700

    update

--- transcode.py
+++ transcode.py
@@ -1,6 +1,6 @@
 import os, ffmpeg, json, time, datetime
 from multiprocessing import Process
-from colorama import Fore, Style
+from colorama import Fore
 
 
 # load config
@@ -28,43 +28,43 @@ def transcode(in_file: str, out_file: str):
         if diff < 1:
             ratio = os.path.getsize(out_file) / os.path.getsize(in_file)
             msg(
-                f"Skipping {in_file}, Compression ratio {ratio * 100:.0f}%, diff {diff:.2f}s"
+                f"[{count}/{total}] Skipping {in_file}, Compression ratio {ratio * 100:.0f}%, diff {diff:.2f}s"
             )
             return
     except:
         pass
     # transcode
     start_time = time.perf_counter()
-    msg(f"{Fore.GREEN}Transcoding {in_file}{Style.RESET_ALL}")
+    msg(f"{Fore.GREEN}[{count}/{total}]Transcoding {in_file} {Fore.RESET}")
     # note that cuda decoding might have problem with h264
-    ffmpeg.input(in_file).output(
-        out_file, vcodec="av1_nvenc", cq=CONSTANT_QUALITY, acodec="copy"
-    ).run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
+    try:
+        ffmpeg.input(in_file).output(
+            out_file, vcodec="av1_nvenc", cq=CONSTANT_QUALITY, acodec="copy"
+        ).run(capture_stdout=True, capture_stderr=True, overwrite_output=True)
+    except Exception as e:
+        msg(f"{Fore.RED}Error transcoding {in_file}: {repr(e)}{Fore.RESET}")
+        msg(f"{Fore.RED}Error message: {e.stderr.decode('utf-8')}{Fore.RESET}")  # type: ignore
+        if os.path.exists(out_file):
+            os.remove(out_file)
+            msg(f"{Fore.RED}Removed {out_file}{Fore.RESET}")
+        return
     end_time = time.perf_counter()
     speed = duration / (end_time - start_time)
     # check if the durations match
     probe = ffmpeg.probe(out_file)
     out_duration = float(probe["format"]["duration"])
     if abs(duration - out_duration) > 1:
-        msg(
-            f"{Fore.RED}Duration diff: {abs(duration - out_duration):.2f}s {in_file}{Style.RESET_ALL}"
-        )
+        msg(f"{Fore.RED}Duration diff: {abs(duration - out_duration):.2f}s {in_file}{Fore.RESET}")
     else:
-        msg(
-            f"{Fore.GREEN}Duration diff: {abs(duration - out_duration):.2f}s {in_file}{Style.RESET_ALL}"
-        )
+        msg(f"{Fore.GREEN}Duration diff: {abs(duration - out_duration):.2f}s {in_file}{Fore.RESET}")
     # check compression ratio
     ratio = os.path.getsize(out_file) / os.path.getsize(in_file)
     if ratio < 1:
-        msg(
-            f"{Fore.GREEN}Compressed {in_file} {ratio * 100:.0f}%, Speed {speed:.1f}X{Style.RESET_ALL}"
-        )
+        msg(f"{Fore.GREEN}Compressed {in_file} {ratio * 100:.0f}%, Speed {speed:.1f}X{Fore.RESET}")
     else:
-        msg(
-            f"{Fore.YELLOW}Compressed {in_file} {ratio * 100:.0f}%, Speed {speed:.1f}X{Style.RESET_ALL}"
-        )
+        msg(f"{Fore.YELLOW}Compressed {in_file} {ratio * 100:.0f}%, Speed {speed:.1f}X{Fore.RESET}")
         os.remove(out_file)
-        msg(f"{Fore.YELLOW}Removed {out_file}{Style.RESET_ALL}")
+        msg(f"{Fore.YELLOW}Removed {out_file}{Fore.RESET}")
 
 
 def traverse(dir: str, args: list):
@@ -99,9 +99,12 @@ args.sort(key=lambda x: x[0])
 # transcode in parallel
 num_proc = 4
 procs = []
+total = len(args)
+count = 0
 while args:
     if len(procs) < num_proc:
         proc = Process(target=transcode, args=args.pop(0))
+        count += 1
         proc.start()
         procs.append(proc)
     else:
@@ -109,9 +112,11 @@ while args:
             if not procs[i].is_alive():
                 procs[i].join()
                 proc = Process(target=transcode, args=args.pop(0))
+                count += 1
                 proc.start()
                 procs[i] = proc
                 break
+    time.sleep(1)
 for proc in procs:
     proc.join()
 msg("Done")
